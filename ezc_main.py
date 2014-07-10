e76b401e5e89a72073409ba6309946c4fb0a28ea
UMueller <Ulimllr@googlemail.com>
2014-07-10 16:22:04 +0200

    sentences are wrapped at 80 characters; more comments in the code

--- ezc_main.py
+++ ezc_main.py
@@ -4,106 +4,91 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from PyQt4.uic import *
 
-from ezc_ui import Ui_Form
 
-def Nicks_first_gui():
-  app = QtGui.QApplication(sys.argv)
-  myapp = MyForm()
-  myapp.show()
-  sys.exit(app.exec_())
+def ezc_Gui():
 
-class MyForm(QtGui.QMainWindow):
+  # functions called by the GUI
+  def message_send():
+      message=str(chatwindow.textEdit.toPlainText())
 
-  """Docstring for MyForm. """
+      f = open(history_path, 'a')
+      f.write("\n"+user+":\n"+message+"\n")
+      f.close()
 
-  def __init__(self, parent = None):
-    """@todo: to be defined1.
+      chatwindow.textEdit.clear()
+      newpost=[user,message]
+      history_modell.data.append(QtCore.QVariant(newpost))
+      history_modell.emit(QtCore.SIGNAL("layoutChanged()"))
 
-    :parent: @todo
+  # variables
+  history_path = os.path.join(os.path.dirname(__file__), 'history',
+                              'history_database.txt')
+  gui_path= os.path.join(os.path.dirname(__file__), 'ui02.ui')
+  group_members=["Nick","Bijan","Gui","Uli"]
+  user="Ul]["
 
-    """
-    QtGui.QMainWindow.__init__(self, parent)
-    self.ui = Ui_Form()
-    self.ui.setupUi(self)
-    QtCore.QObject.connect( self.ui.OpenFile, QtCore.SIGNAL("clicked()"),
-                            self.file_dialog )
-    QtCore.QObject.connect( self.ui.textIn, QtCore.SIGNAL("returnPressed()"),
-                            self.copyInToOut )
-  def file_dialog(self):
-    """@todo: Docstring for file_dialog.
-    :returns: @todo
+  #loading the GUI
+  app = QApplication(sys.argv)
+  chatwindow = loadUi(gui_path)
 
-    """
-    fd = QtGui.QFileDialog(self)
-    plik = open(fd.getOpenFileName()).read()
-    self.ui.textOut.setText(plik)
+  # Data are imported from the database into a Modell class
+  history_modell = Modell(history_path)
 
-  def copyInToOut(self):
-    """@todo: Docstring for copyInToOut.
-    :returns: @todo
-
-    """
-    if self.ui.textIn.text():
-      self.ui.textOut.append(self.ui.textIn.text())
-
-    self.ui.textIn.clear()
+  # Data are transfered from the modell class to the View class
+  show_history = View(history_modell,chatwindow.listView)
 
 
+  # connections and actions
+  chatwindow.connect(chatwindow.Send_Button, SIGNAL("released()"),
+                     message_send)
 
-def Ulis_first_gui():
-    history_path = os.path.join(os.path.dirname(__file__), 'history',
-            'history_database.txt')
-    gui_path= os.path.join(os.path.dirname(__file__), 'ui02.ui')
-
-    def message_send():
-        message=str(chatwindow.textEdit.toPlainText())
-
-        f = open(history_path, 'a')
-        f.write(user+":\n"+message+"\n\n")
-        f.close()
-
-        chatwindow.textEdit.clear()
-        newpost=[user,message]
-        history_modell.datensatz.append(QtCore.QVariant(newpost))
-        history_modell.emit(QtCore.SIGNAL("layoutChanged()"))
-
-    history_modell = Modell(history_path)
-    group_members=["Nick","Bijan","Gui","Uli"]
-    user="Ul]["
-    app = QApplication(sys.argv)
-    chatwindow = loadUi(gui_path)
-    show_history = View(history_modell,chatwindow.listView)
-    chatwindow.connect(chatwindow.Send_Button, SIGNAL("released()"), message_send)
-
-    chatwindow.show()
-    app.exec_()
+  # execute application
+  chatwindow.show()
+  app.exec_()
 
 
 class Modell(QtCore.QAbstractListModel):
     def __init__(self, dateiname):
         QtCore.QAbstractListModel.__init__(self)
-        self.datensatz = []
+        self.data = []
 
-        # Lade Datensatz
+        word_wrap_length=80           # we could define this later as a variable
+        # Lade data
         f = open(dateiname)
+
         try:
             lst = []
             for zeile in f:
                 if not zeile.strip():
-                    self.datensatz.append(QtCore.QVariant(lst))
+                    self.data.append(QtCore.QVariant(lst))
                     lst = []
                 else:
-                    lst.append(zeile.strip())
+                    if len(zeile.strip())>word_wrap_length and len(zeile.split())>1:
+                        sum=0
+                        line=""
+                        list_of_words=zeile.split()
+                        for i in range(len(list_of_words)):
+                          sum+=len(list_of_words[i])+1
+                          if sum<word_wrap_length:
+                            line+=list_of_words[i]+" "
+                          else:
+                            sum=len(list_of_words[i])
+                            lst.append(QtCore.QVariant(line))
+                            line=list_of_words[i]+" "
+                        if line:
+                          lst.append(QtCore.QVariant(line))
+                    else:
+                      lst.append(zeile.strip())
             if lst:
-                self.datensatz.append(QtCore.QVariant(lst))
+                self.data.append(QtCore.QVariant(lst))
         finally:
             f.close()
 
     def rowCount(self, parent=QtCore.QModelIndex()):
-        return len(self.datensatz)
+        return len(self.data)
 
     def data(self, index, role=QtCore.Qt.DisplayRole):
-        return QtCore.QVariant(self.datensatz[index.row()])
+        return QtCore.QVariant(self.data[index.row()])
 
 class View(QtGui.QListView):
    def __init__(self, modell, parent=None):
@@ -146,7 +131,7 @@ class ViewDelegate(QtGui.QItemDelegate):
         rahmenTitel.setHeight(self.titelHoehe)
         rahmenTitelText = rahmenTitel.adjusted(self.abstandText,
                                         0, self.abstandText, 0)
-        datensatz = index.data().toList()
+        data = index.data().toList()
         painter.save()
         painter.setPen(self.rahmenStift)
         # painter.drawRect(rahmen)
@@ -157,18 +142,16 @@ class ViewDelegate(QtGui.QItemDelegate):
         painter.setFont(self.titelSchriftart)
         painter.drawText(rahmenTitelText,
                     QtCore.Qt.AlignLeft | QtCore.Qt.AlignVCenter,
-                    datensatz[0].toString())
+                    data[0].toString())
 
         # Adresse schreiben
         painter.setPen(self.textStift)
         painter.setFont(self.textSchriftart)
-        for i, eintrag in enumerate(datensatz[1:]):
+        for i, eintrag in enumerate(data[1:]):
             painter.drawText(rahmenTitel.x() + self.abstandText,
                    rahmenTitel.bottom() + (i+1)*self.zeilenHoehe,
                    "%s" % eintrag.toString())
         painter.restore()
 
 if __name__ == "__main__":
-    #Nicks_first_gui()
-    Ulis_first_gui()
-
+    ezc_Gui()
