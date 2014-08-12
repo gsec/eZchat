b1efe71a053edc2051060f6b2785f0ac1e84fac6
Nick <jlang@physik.uni-wuerzburg.de>
2014-08-12 16:53:06 +0200

    contactlist: user selectable & online-state box. No functionality yet included

--- ez_cli.py
+++ ez_cli.py
@@ -33,6 +33,43 @@ class VimButton(urwid.Button):
       urwid.Button.keypress(size, key)
 
 #==============================================================================#
+#                                  VimListBox                                  #
+#==============================================================================#
+
+class VimListBox(urwid.ListBox):
+
+  def __init__(self, *args, **kwargs):
+    urwid.ListBox.__init__(self, *args, **kwargs)
+    self.command_dict = {'j' : self.cmd_move_down,
+                         'k' : self.cmd_move_up,
+                         'down' : self.cmd_move_down,
+                         'up' : self.cmd_move_up,
+                         'q': self.cmd_close_list,
+
+                         # blocking the left & right arrow key.
+                         'left' : self.cmd_unhandled,
+                         'right' : self.cmd_unhandled,
+                         }
+
+  def cmd_unhandled(self, *args):
+    pass
+
+  def cmd_move_up(self, size):
+    urwid.ListBox.keypress(self, size, 'up')
+
+  def cmd_move_down(self, size):
+    urwid.ListBox.keypress(self, size, 'down')
+
+  def cmd_close_list(self, *args):
+    ez_cli.top.close_box()
+
+  def keypress(self, size, key):
+    try:
+      return self.command_dict[key](size)
+    except KeyError:
+      return urwid.ListBox.keypress(self, size, key)
+
+#==============================================================================#
 #                                VimCommandLine                                #
 #==============================================================================#
 
@@ -58,7 +95,8 @@ class VimCommandLine(urwid.Edit):
                          "send" : cl.cl.cmd_send,
                          "quit" : self.cmd_close,
                          "q" : self.cmd_close,
-                         "show" : self.cmd_show
+                         "show" : self.cmd_show,
+                         "open" : self.cmd_open
                         }
 
   def cmd_show(self, file_name):
@@ -67,8 +105,27 @@ class VimCommandLine(urwid.Edit):
     self.vimedit.initialized = True
     return
 
+  def cmd_open(self, *args):
+    assert(args[0] == 'contacts')
+    #contacts   = VimText(caption=('VimEdit', u'eZchat\n\n'))
+    #contacts_f = urwid.Filler(contacts, valign = 'top')
+    def contact_list(user_ids):
+      contacts = [urwid.Text("contacts")]
+      for user_id in user_ids:
+        on  = urwid.Text(("online",u"ON"))
+        off = urwid.Text(("offline", u"OFF"))
+        contacts += [urwid.Columns([urwid.CheckBox(user_id), off])]
+      return VimListBox(urwid.SimpleListWalker(contacts))
+
+    lst = contact_list(["Alice", "Bob"])
+    #lst_f = urwid.Filler(lst, valign = 'top')
+    ez_cli.top.open_box(lst, 50)
+    #ez_cli.top.open_box(contacts_f)
+    return
+
   def cmd_close(self):
-    urwid.emit_signal(self, 'exit_ez_chat')
+    ez_cli.top.close_box()
+    #urwid.emit_signal(self, 'exit_ez_chat')
     return
 
   def tab_completion(self):
@@ -115,6 +172,68 @@ class VimCommandLine(urwid.Edit):
     # do not allow to delete `:`
     elif key != 'backspace' or p > 1:
       urwid.Edit.keypress(self, size, key)
+
+
+class VimText(urwid.Edit):
+  def __init__(self, *args, **kwargs):
+    urwid.Edit.__init__(self, *args, **kwargs)
+    self.command_dict = {'h' : self.cmd_move_left,
+                         'l' : self.cmd_move_right,
+                         'j' : self.cmd_move_down,
+                         'k' : self.cmd_move_up,
+                         'down' : self.cmd_move_down,
+                         'up' : self.cmd_move_up,
+                         'left' : self.cmd_move_left,
+                         'right' : self.cmd_move_right,
+                        }
+
+  def cmd_move_left(self):
+    p = move_prev_char(self.edit_text,0,self.p)
+    self.set_edit_pos(p)
+
+  def cmd_move_right(self):
+    p = move_next_char(self.edit_text,self.p,len(self.edit_text))
+    self.set_edit_pos(p)
+
+  def cmd_move_down(self, shift=1):
+    #self.highlight = None
+    x, y = self.get_cursor_coords((self.maxcol,))
+    pref_col = self.get_pref_col((self.maxcol,))
+    y += shift
+    # ?
+    if not self.move_cursor_to_coords((self.maxcol,), pref_col, y):
+      if shift == 1:
+        return 'down'
+      else:
+        return 'up'
+
+  def cmd_move_up(self):
+    self.cmd_move_down(shift=-1)
+
+  def keypress(self, size, key):
+    (self.maxcol,) = size
+    self.p = self.edit_pos
+
+    # send message
+    if key == 'enter':
+      return
+
+    # execute commands
+    elif key in self.command_dict:
+      try:
+        self.command_dict[key]()
+      except KeyError:
+        pass
+
+    elif key == 'esc':
+      self.set_edit_text("esc")
+      ez_cli.top.close_box()
+      return
+
+    else:
+      urwid.Edit.keypress(self, size, key)
+
+
 
 #==============================================================================#
 #                                   VimEdit                                    #
@@ -263,6 +382,8 @@ class VimEdit(urwid.Edit):
     elif self.mode == VimEdit.insert_mode:
       urwid.Edit.keypress(self, size, key)
 
+
+
 #==============================================================================#
 #                                 ez_cli_urwid                                 #
 #==============================================================================#
@@ -280,7 +401,33 @@ class ez_cli_urwid(urwid.Frame):
     self.vimedit_f     = urwid.Filler(self.vimedit, valign = 'top')
     self.commandline_f = urwid.Filler(self.commandline, valign = 'bottom')
 
-    urwid.Frame.__init__(self, self.vimedit_f, footer=self.commandline)
+    focus_map = {
+    'heading': 'focus heading',
+    'options': 'focus options',
+    'line': 'focus line'}
+
+    class HorizontalBoxes(urwid.Columns):
+      def __init__(self):
+        super(HorizontalBoxes, self).__init__([], dividechars=1)
+
+      def open_box(self, box, weight):
+        if self.contents:
+          del self.contents[self.focus_position + 1:]
+
+        self.contents.append((urwid.AttrMap(box, 'options', focus_map),
+            self.options('weight', weight)))
+        self.focus_position = len(self.contents) - 1
+
+      def close_box(self):
+        del self.contents[1]
+        self.focus_position = 0
+
+
+    self.top = HorizontalBoxes()
+    self.top.open_box(self.vimedit_f, 100)
+    self.top_f = urwid.Filler(self.top, 'top', 20)
+
+    urwid.Frame.__init__(self, self.top_f, footer=self.commandline)
 
     urwid.connect_signal(self.vimedit, 'done', self.mode_notifier)
     urwid.connect_signal(self.vimedit, 'insert_mode', self.mode_notifier)
@@ -313,7 +460,11 @@ class ez_cli_urwid(urwid.Frame):
 #==============================================================================#
 
 ez_cli = ez_cli_urwid()
-loop = urwid.MainLoop(ez_cli)
+palette = [
+    ('online', 'light green', 'dark green'),
+    ('offline', 'dark red', 'light red'),
+    ]
+loop = urwid.MainLoop(ez_cli, palette)
 
 def received_output(data):
   ez_cli.vimedit.set_edit_text(ez_cli.vimedit.get_edit_text() + data)
