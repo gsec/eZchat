b17dba0c0d15ebeba32756c7bc3d38b9da386411
Nick <jlang@physik.uni-wuerzburg.de>
2014-10-10 00:58:56 +0200

    separated client from internal/simple cli

---
+++ ez_simple_cli.py
@@ -0,0 +1,173 @@
+#==============================================================================#
+#                               ez_simple_cli.py                               #
+#==============================================================================#
+
+#============#
+#  Includes  #
+#============#
+
+import sys
+import cPickle as pickle
+from ez_process import p2pCommand
+
+#==============================================================================#
+#                               class simple_cli                               #
+#==============================================================================#
+
+class ez_simple_cli(object):
+
+  def __init__(self, *args, **kwargs):
+    super(simple_cli, self).__init__(*args, **kwargs)
+
+#=======================#
+#  simple build-in cli  #
+#=======================#
+
+# TODO: nick separate cli Do 07 Aug 2014 00:32:46 CEST
+# The commands do only require the access to the commandQueue
+# allowing to separate the cli from client
+
+# TODO: nick send command Do 07 Aug 2014 00:37:22 CEST
+# The send command still uses direct access to the clients socket -> should be
+# done via commandQueue
+
+  def CLI(self):
+    data = sys.stdin.readline()
+    if not data:
+      return
+
+#=================#
+#  close process  #
+#=================#
+    if str(data[:-1]) == "close":
+      self.enableCLI = False
+      self.commandQueue.put(p2pCommand('shutdown'))
+      return
+
+#===========================#
+#  online users + contacts  #
+#===========================#
+    elif str(data[:-1]) == "users":
+      print "online users"
+      for user in self.ips:
+        print "user:", user, self.ips[user]
+      print "contacts"
+      UIDs = self.UserDatabase.UID_list()
+      for entry in self.UserDatabase.get_entries(UIDs):
+        print "contact:", entry.name
+
+#================#
+#  ping process  #
+#================#
+    elif "ping" in str(data[:-1]):
+      try:
+        #_, user_id = data.split()
+        bp_ping = p2pCommand('ping_background')
+        self.commandQueue.put(bp_ping)
+        #self.commandQueue.put(p2pCommand('ping_request', user_id))
+      except:
+        self.replyQueue.put(self.error("Syntax error in ping"))
+
+#============================#
+#  add user to online users  #
+#============================#
+    elif "add" in str(data[:-1]):
+      try:
+        _, user_id, host, port = data.split()
+        self.handlers['add_client'](user_id, (str(host), int(port)))
+        self.commandQueue.put(p2pCommand('ping_request', user_id))
+      except:
+        self.replyQueue.put(self.error("Syntax error in user"))
+
+#===================#
+#  start listening  #
+#===================#
+    elif "servermode" in str(data[:-1]):
+      try:
+        _, host, port = data.split()
+        self.commandQueue.put(p2pCommand('servermode', (host, int(port))))
+      except:
+        self.replyQueue.put(self.error("Syntax error in servermode"))
+
+#=====================================#
+#  show running background processes  #
+#=====================================#
+    elif "bp" in str(data[:-1]):
+      try:
+        print ("background_processes:", self.background_processes)
+      except:
+        self.replyQueue.put(self.error("Syntax error in bp"))
+
+    elif "sync" in str(data[:-1]):
+      try:
+        _, user_id = data.split()
+        self.commandQueue.put(p2pCommand('db_sync_request_out', user_id))
+      except:
+        self.replyQueue.put(self.error("Syntax error in ips"))
+
+
+#==================================================#
+#  user requests connection with all online users  #
+#==================================================#
+    elif "ips" in str(data[:-1]):
+      users = data.split()
+      try:
+        if len(users) > 1:
+          for user_id in users[1:]:
+            cmd_dct = {'user_id': user_id}
+            self.commandQueue.put(p2pCommand('ips_request', cmd_dct))
+        else:
+          for user_id in self.ips:
+            cmd_dct = {'user_id': user_id}
+            self.commandQueue.put(p2pCommand('ips_request', cmd_dct))
+
+      except:
+        self.replyQueue.put(self.error("Syntax error in ips"))
+
+#============================#
+#  add user to contact list  #
+#============================#
+    elif "key" in str(data[:-1]):
+      try:
+        _, user_id = data.split()
+        cmd_dct = {'user_id': user_id}
+        self.commandQueue.put(p2pCommand('contact_request_out', cmd_dct))
+      except:
+        self.replyQueue.put(self.error("Syntax error in key"))
+
+#========================#
+#  verify send packages  #
+#========================#
+    elif "verify" in str(data[:-1]):
+      for key in self.stored_packets:
+        packets = self.stored_packets[key]
+        reconstructed, result = packets.reconstruct_data()
+        if not reconstructed:
+          for packet_number in result:
+            cmd = p2pCommand('packet_request',
+                             ((packets.packets_hash, packet_number), key[0]))
+            self.commandQueue.put(cmd)
+        else:
+          print "package:", key, " successfully reconstructed"
+
+#======================#
+#  send encrypted msg  #
+#======================#
+    else:
+      try:
+        user_id, msg = data.split()
+        if not self.UserDatabase.in_DB(name=user_id):
+          return
+
+        mx = em.Message(self.name, user_id, msg)
+        self.MsgDatabase.add_entry(mx)
+
+        if not user_id in self.ips:
+          return
+
+        data = pickle.dumps(mx)
+        cmd_data = {'user_id': user_id, 'data':data}
+        self.commandQueue.put(p2pCommand('send', cmd_data))
+
+      except:
+        self.replyQueue.put(self.error("Syntax error in command"))
