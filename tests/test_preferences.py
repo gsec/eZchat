939dfd234bdc8f6f4de7f0b9333c959cfc13d1e4
gsec <o0v0o.ix@gmail.com>
2014-10-13 20:22:50 +0200

    removed preferences-location test

--- tests/test_preferences.py
+++ tests/test_preferences.py
@@ -8,16 +8,16 @@ from shutil import rmtree
   # TODO: (bcn 2014-08-01) How to test raw_input ?
   #ep.check_key_location()
 
-def test_return_location():
-  ep.local = 'tests/local'
-  if not ep.path.isdir(ep.local):
-    ep.makedirs(ep.local)
-  for var in [ep.key_loc, ep.hist_loc, ep.log_loc]:
-    print("Checking for " + var[1] + " directory.")
-    #ep.makedirs(var[0])       # remove this line for testing of raw input
-    print(ep.return_location(var, test_func=(lambda:"y")))
-    if ep.path.isdir(var[0]):
-      print( var[0] + " location exists now.")
-  # dont delete non-test directories
-  assert '/tests/' in ep.path.abspath(ep.local)
-  rmtree(ep.local)
+# def test_return_location():
+#   ep.local = 'tests/local'
+#   if not ep.path.isdir(ep.local):
+#     ep.makedirs(ep.local)
+#   for var in [ep.key_loc, ep.hist_loc, ep.log_loc]:
+#     print("Checking for " + var[1] + " directory.")
+#     #ep.makedirs(var[0])       # remove this line for testing of raw input
+#     print(ep.return_location(var, test_func=(lambda:"y")))
+#     if ep.path.isdir(var[0]):
+#       print( var[0] + " location exists now.")
+#   # dont delete non-test directories
+#   assert '/tests/' in ep.path.abspath(ep.local)
+#   rmtree(ep.local)
